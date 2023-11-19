50b3b72a096af99a2a110a0b2e8c224e89b6b461
Moronfolu Olufunke <moronfolu.motunrayo1@gmail.com>
2023-11-19 02:47:52 +0100

    displaying the nomination count in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { fetchCubeAcademyGetAllNominations } from "../../api/nominationsComponents";
+import { getAuthToken } from "../../utils/authHelper";
 import cubelogo from "../../assets/3sc-logo.svg";
 import folder from "../../assets/folder.svg";
 import emblem from "../../assets/emblem.svg";
 import plus from "../../assets/plus.svg";
 
 const Header = () => {
+	const [numberOfNominations, setNumberOfNominations] = useState(0);
+	useEffect(() => {
+		const fetchNominationsCount = async () => {
+			try {
+				const count = await fetchCubeAcademyGetAllNominations({
+					headers: {
+						Authorization: `Bearer ${getAuthToken()}`,
+					},
+				});
+
+				setNumberOfNominations(count.data.length);
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+
+		fetchNominationsCount();
+	}, [numberOfNominations]);
 	return (
 		<header className="bg-black flex justify-between items-center px-5 py-4 md:py-0">
-			<img src={cubelogo} alt="" className="hidden md:block" />
-			<img src={emblem} alt="" className="block md:hidden" />
+			<Link to="/">
+				<img src={cubelogo} alt="" className="hidden md:block" />
+			</Link>
+			<Link to="/">
+				<img src={emblem} alt="" className="block md:hidden" />
+			</Link>
+
 			<div>
 				<img
 					src={plus}
@@ -24,7 +50,7 @@ const Header = () => {
 			<Link
 				to="/view-nominations"
 				className="text-white underline text-xs font-AnonymousPro hidden md:block">
-				Your nominations ()
+				Your nominations ({numberOfNominations})
 			</Link>
 		</header>
 	);
